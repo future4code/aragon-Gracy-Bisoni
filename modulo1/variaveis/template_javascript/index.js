ed1e467b8f770430043eede70ede0ab676a747e0
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-03-21 15:11:22 -0300

    Atividade Variáveis + Desafio

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,49 @@
+//Exercício 1
+let nome
+let idade
+console.log(typeof idade)
+console.log(typeof nome)
+// como não foram atribuídos valores nas variáveis, o JS retornou indefinido para o tipo da variável.
+
+//nome=prompt("Qual é o seu nome?")
+//idade=prompt("Qual a sua idade?")
+
+console.log(nome)
+console.log(idade)
+console.log(typeof idade)
+console.log(typeof nome)
+// vieram strings porque são respostas a um prompt
+
+console.log("Olá "+nome+", você tem "+idade+" anos")
+
+//Exercício 2
+const pergunta1 = "Você gosta de paçoca?"
+const pergunta2 = "Hoje está frio?"
+const pergunta3 = "Seu nome é Cleiton?"
+
+//const ispacoca = prompt(pergunta1)
+//const isclima = prompt(pergunta2)
+//const iscleiton = prompt(pergunta3)
+
+//console.log(pergunta1+" - "+ispacoca)
+//console.log(pergunta2+" - "+isclima)
+//console.log(pergunta3+" - "+iscleiton)
+
+//Exercício 3
+let a = 10
+let b = 25
+let c = a
+a=b
+b=c
+//console.log("O novo valor de a é",a)
+//console.log("O novo valor de b é",b)
+
+//Desafio
+//let n1 = Number(prompt("Lança um número inteiro"))
+//let n2 = Number(prompt("Mais um (número inteiro)"))
+
+let x = n1+n2
+console.log(x)
+let y = n1*n2
+console.log(y)
+
