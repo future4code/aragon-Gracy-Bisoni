fc0be9d67d4ef1f54c9c080e797d22dcbb311e17
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-03-24 16:54:58 -0300

    Atividades Funções + Desafio 1a

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,102 @@
+// Interpretação de código
+// 1(a) 10 e 50, um abaixo do outro (b) Nada. Sem console.log não tem impressão de conteúdo no console.
+// 2(a) Coloca o texto do usuário todo em letras minúsculas e procura a palavra "cenoura" na string/frase. Depois, foi criada uma variável com o conteúdo da função e impressa no console.
+// (b) i. eu gosto de cenoura, TRUE; ii. cenoura é bom pra vista, TRUE; iii. cenouras crescem na terra, FALSE
+
+// Escrita de código
+//----------------------- Exercício 1 -------------------------
+// (a)
+// function imprimir() {
+//     return console.log("Eu sou Isabelle, tenho 24 anos, moro em Colombo e sou estudante.")
+// }
+// imprimir()
+
+//(b)
+// let nome = prompt("Qual é o seu nome?")
+// let idade = Number(prompt("Qual é a sua idade?"))
+// let cidade = prompt("Qual cidade você mora?")
+// let ocupacao = prompt("Qual é a sua ocupação atual?")
+
+// function frase (nome,idade,cidade,ocupacao){
+//     return console.log (`Eu sou ${nome},tenho ${idade} anos, moro em ${cidade} e sou ${ocupacao}`)
+// }
+
+// frase(nome,idade,cidade,ocupacao)
+
+//--------------------- Exercício 2 ----------------------
+//(a)
+// let num1 = Number(prompt("Escreva um número aqui"))
+// let num2 = Number(prompt("Escreva mais um número"))
+// function soma(num1,num2){
+//  return num1+num2 
+// }
+// console.log(soma(num1,num2))
+
+// //(b)
+// function maiorOuIgual (num1,num2){
+//     return num1>=num2
+// }
+// console.log(maiorOuIgual(num1,num2))
+
+//(c) 
+// function parOuImpar (num1){
+// let divisao = num1%2
+// return divisao === 0
+// }
+// console.log(parOuImpar(num1))
+
+//(d)
+// let frase = prompt("Escreva aqui uma frase qualquer")
+
+// function toUpperCaseETamanho (frase){
+// let caixaAlta = frase.toUpperCase()
+// let tamanhoDaString = frase.length
+// return console.log(caixaAlta,tamanhoDaString)
+// }
+// toUpperCaseETamanho(frase)
+
+//--------------------- Exercício 3 --------------------------
+// function entradas (num1,num2){
+//     return console.log (`Números inseridos: ${num1},${num2}`)
+// }
+
+// function soma (num1,num2){
+//    let soma = num1+num2
+//    return console.log (`Soma:${soma}`)
+// }
+
+// function diferenca(num1,num2){
+//     let diferenca = num1-num2
+//     return console.log(`Diferença: ${diferenca}`)
+// }
+
+// function multi (num1,num2){
+//     let produto = num1*num2
+//     return console.log(`Multiplicação:${produto}`)
+// }
+
+// function divisao (num1,num2){
+//     let divisao = num1/num2
+//     return console.log(`Divisão:${divisao}`)
+// }
+
+// let num1 = Number(prompt("Escreva um número aqui"))
+// let num2 = Number(prompt("Escreva mais um número"))
+// entradas(num1,num2)
+// soma(num1,num2)
+// diferenca(num1,num2)
+// multi(num1,num2)
+// divisao(num1,num2)
+
+// ---------------------- Desafio 1 -------------------------------
+//(a)
+// let numeroQualquer = Number(prompt("Escreva um número qualquer"))
+// const imprime = (numeroQualquer) => {
+//     return console.log(numeroQualquer)
+// }
+// imprime(numeroQualquer)
+
+//(b)
+
+
+
