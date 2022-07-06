18967bcf6354254feb575ebe319348725ee4bb56
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-07-06 16:39:53 -0300

    feat: exercícios 1-6

---
+++ modulo4/dataflow-backend/src/data.ts
@@ -0,0 +1,33 @@
+export type Product = {
+    name:string,
+    id:number,
+    price:number
+}
+
+export const products:Product[] = [
+    {
+        name: 'boots',
+        id: 1,
+        price: 29.90
+    },
+    {
+        name: 'cardigan',
+        id: 2,
+        price: 59.90
+    },
+    {
+        name: 'socks',
+        id: 3,
+        price: 9.90
+    },
+    {
+        name: 'panties',
+        id: 4,
+        price: 19.90
+    },
+    {
+        name: 'T-shirt',
+        id: 5,
+        price: 39.90
+    }
+]
