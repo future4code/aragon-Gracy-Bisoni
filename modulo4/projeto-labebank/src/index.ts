a0c4ed94b3bc3a8c3896e359f90fa670292c8b8b
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-07-10 10:07:58 -0300

    feat: exercício 4

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -40,7 +40,7 @@ app.post('/users', (req: Request, res: Response) => {
         }
 
         if (indexCPF < 0) {
-            const newUser:User= {
+            const newUser: User = {
                 id: users.length + 1,
                 name,
                 CPF,
@@ -50,7 +50,7 @@ app.post('/users', (req: Request, res: Response) => {
             }
 
             users.push(newUser)
-        } else{
+        } else {
             res.statusCode = 409
             throw new Error("CPF already exists");
         }
@@ -82,75 +82,75 @@ app.get('/users', (req: Request, res: Response) => {
 })
 
 // Pega o saldo do usuário passado pelo path (Return balance of client informed by ID)
-app.get('/users/balance/:id', (req: Request, res: Response)=> {
+app.get('/users/balance/:id', (req: Request, res: Response) => {
     try {
         const id = Number(req.params.id)
-    const userIndex = users.findIndex(user => user.id === id)
+        const userIndex = users.findIndex(user => user.id === id)
 
-    if(!id){
-        res.statusCode = 422
-        throw new Error("Need to add a valid ID, number type");
-    }
+        if (!id) {
+            res.statusCode = 422
+            throw new Error("Need to add a valid ID, number type");
+        }
 
-    if(userIndex < 0){
-        res.statusCode = 404
-        throw new Error("User not found")
-    }
+        if (userIndex < 0) {
+            res.statusCode = 404
+            throw new Error("User not found")
+        }
 
-    const result = users.filter((user)=> {
-        return user.id === id
-    }).map((user)=> {
-        return user.balance
-    })
+        const result = users.filter((user) => {
+            return user.id === id
+        }).map((user) => {
+            return user.balance
+        })
 
-    res.status(200).send({
-        user: users[userIndex].name,
-        balance: `R$${result},00`
-    })
+        res.status(200).send({
+            user: users[userIndex].name,
+            balance: `R$${result},00`
+        })
     } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
 // Adiciona um valor ao saldo (Add a value to balance)
-app.put('/users/:id', (req: Request, res: Response)=> {
+app.put('/users/:id', (req: Request, res: Response) => {
     try {
         const id = Number(req.params.id)
-        const {balance} = req.body
+        const { balance } = req.body
         const userIndex = users.findIndex(user => user.id === id)
 
-        if(!id || !balance){
-            res.statusCode=422
+        if (!id || !balance) {
+            res.statusCode = 422
             throw new Error("Insert a valid ID and balance value");
         }
 
-        if(typeof balance !== 'number'){
+        if (typeof balance !== 'number') {
             res.statusCode = 422
             throw new Error("Balance needs to be number type");
-        } else if (balance <= 0){
+        } else if (balance <= 0) {
             res.statusCode = 422
             throw new Error("Balance value must be greater than zero");
         }
 
-        if(userIndex < 0) {
+        if (userIndex < 0) {
             res.statusCode = 404
             throw new Error("User not found");
         } else {
 
-            const result = users.filter((user)=> {
-                if (user.id === id){
+            const result = users.filter((user) => {
+                if (user.id === id) {
                     return user
                 }
-            }).map((user)=> {
+            }).map((user) => {
                 user.balance += balance
                 return user
             })
-            
+
             res.status(200).send({
                 message: "Success",
                 newBalance: `R$ ${result[userIndex].balance},00`,
                 user: result[userIndex]
-            }) 
+            })
         }
 
     } catch (error) {
@@ -159,8 +159,79 @@ app.put('/users/:id', (req: Request, res: Response)=> {
 })
 
 // Pay a bill
-app.put('/users/:id/pay', (req:Request, res: Response)=> {
+app.put('/users/:id/pay', (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id)
+        const { description, value } = req.body
+        const userIndex = users.findIndex(user => user.id === id)
     
+        function formatDate (){
+            const dayString = new Date().getDate().toString()
+            const currentDay = dayString.length === 1? '0'+dayString : dayString
+            const monthString = new Date().getMonth().toString()
+            const currentMonth = monthString.length === 1? '0'+monthString : monthString
+            const currentYear = new Date().getFullYear().toString()
+
+            const date = `${currentDay}/${currentMonth}/${currentYear}`
+
+            return date
+        }
+
+
+        if (!value || !description || !id) {
+            res.statusCode = 422
+            throw new Error("Insert a valid ID, value and description");
+        }
+
+        if (typeof value !== "number") {
+            res.statusCode = 422
+            throw new Error("Value must be number type");
+        }
+
+
+        if (typeof description !== 'string') {
+            res.statusCode = 422
+            throw new Error("Description must be string type");
+        } else {
+            const descriptionSplit = description.split('')
+            if (descriptionSplit.length < 6) {
+                res.statusCode = 422
+                throw new Error("Description must have at least 6 characters");
+            }
+        }
+
+        if (userIndex < 0) {
+            res.statusCode = 404
+            throw new Error("User not found");
+        } else {
+            const newTransaction: Transaction = {
+                value,
+                description,
+                date: formatDate(),
+            }
+            const result = users.map((user) => {
+                if (user.id === id) {
+                    if (user.balance < value) {
+                        res.statusCode = 400;
+                        throw new Error("Customer doesn't have enough balance");
+                    } else {
+                        user.balance -= value
+                        user.statement.push(newTransaction)
+                    }
+                }
+                return user
+            }).filter((user) => {
+                user.id === id
+            })
+            res.status(200).send({
+                message: "You paid the bill successfully!",
+                user: users[userIndex]
+            })
+        }
+
+    } catch (err) {
+        res.status(res.statusCode).send(err.message)
+    }
 })
 
 
