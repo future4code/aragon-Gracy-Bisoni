8c021a31a466604eded30a1092194079a05253be
Isabelle Daru Bisoni <isadarub@gmail.com>
2022-07-15 08:47:20 -0300

    feat: exercício 1

--- modulo4/intro-knex/src/index.ts
+++ modulo4/intro-knex/src/index.ts
@@ -17,7 +17,7 @@ app.get('/produtos',async (req:Request, res:Response)=> {
         if (busca) {
             const [resultado] = await connection.raw(`
                 SELECT * FROM Produtos
-                WHERE nome LIKE "%${busca.toLowerCase()}%";
+                WHERE LOWER(nome) LIKE "%${busca.toLowerCase()}%";
             `)
             return res.status(200).send({produtos: resultado})
         }
@@ -30,6 +30,38 @@ app.get('/produtos',async (req:Request, res:Response)=> {
         res.status(errorCode).send({message: error.message})
     }
 })
+
+// Exercício 1
+app.get('/colaborators', async (req: Request, res: Response)=> {
+    let errorCode = 400
+    try {
+        const busca = req.query.busca as string
+
+        if(busca){
+            const [result] = await connection.raw(`
+                SELECT * FROM Colaborators
+                WHERE LOWER(name) LIKE "%${busca.toLowerCase()}%";
+            `)
+
+            res.status(200).send({
+                result
+            })
+        }
+
+        const [result] = await connection.raw(`
+            SELECT * FROM Colaborators;
+        `)
+
+        res.status(200).send({
+            result
+        })
+        
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+})
+
+
 
 app.listen(process.env.PORT || 3003, () => {
   console.log(`Server running on port ${process.env.PORT || 3003}`)
